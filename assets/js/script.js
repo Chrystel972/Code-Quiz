cff59c916453cbfb7f9e9456fae5e6618428f07a
Chrystel <chrystel.biaka@live.com>
2023-11-01 23:24:21 +0000

    start pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// A start button that when clicked a timer starts and the first question appears.
+// var startQuiz = document.queryselector("start");
+// var startTimer = document.querySelector("timer");
+// var count = 0;
+
+// //create a function to run after the start button has been pressed
+// function startingPoint(){
+// startTimer.addEventListener("click", function() {
+//     startTimer.textContent = count;
+// }), 1000
+// }
+// Questions contain buttons for each answer.
+// When answer is clicked, the next question appears
+// If the answer clicked was incorrect then subtract time from the clock
+// The quiz should end when all questions are answered or the timer reaches 0.
+
+// When the game ends, it should display their score and give the user the ability to save their initials and their score
